d2e2307012b245843ebffc1f2afddc86ab97a6ce
gabriel COUTURE <gabriel.couture@epitech.eu>
2021-10-28 13:55:30 +0200

    add base files for streamlit

---
+++ app.py
@@ -0,0 +1,35 @@
+import streamlit as st 
+import pandas as pd
+import numpy as np
+import os
+import pickle
+import warnings
+
+import matplotlib.pyplot as plt
+import plotly.express as px
+from collections import Counter
+
+from matplotlib.figure import Figure
+import seaborn as sns
+
+
+st.set_page_config(page_title="Heart Disease", page_icon="https://www.freeiconspng.com/thumbs/heart-png/heart-png-15.png", layout='centered', initial_sidebar_state="collapsed")
+
+def main():
+    html_temp = """
+    <div>
+    <h1 style="color:MEDIUMSEAGREEN;text-align:left;"> Crop Recommendation  🌱 </h1>
+    </div>
+    """
+    st.markdown(html_temp, unsafe_allow_html=True)
+
+
+hide_menu_style = """
+        <style>
+        #MainMenu {visibility: hidden;}
+        </style>
+        """
+st.markdown(hide_menu_style, unsafe_allow_html=True)
+
+if __name__ == '__main__':
+	main()
